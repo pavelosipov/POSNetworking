8c3d6fca9803c92be2ef7a427b56a6eb124508f4
Pavel Osipov <p.osipov@corp.mail.ru>
2018-07-04 14:31:25 +0300

    Update POSTesting.h header.

--- Classes/Testing/POSTesting.h
+++ Classes/Testing/POSTesting.h
@@ -1,10 +1,11 @@
 //
-//  POSNetworkingTesting.h
+//  POSTesting.h
 //  POSNetworking
 //
 //  Created by Pavel Osipov on 21/08/17.
 //  Copyright © 2017 Pavel Osipov. All rights reserved.
 //
 
-#import <POSRx/POSHTTPGatewayStub.h>
-#import <POSRx/POSRx.h>
+#import "POSHTTPGatewayStub.h"
+#import "POSHTTPHostStub.h"
+#import "POSNetworking.h"
